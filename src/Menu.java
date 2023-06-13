f7574b8f053c87428187dc20620a016328d8ebed
Juan Diego <juandiegojd252@gmail.com>
2023-06-13 09:54:58 -0500

    ArrayList

--- src/Menu.java
+++ src/Menu.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Menu {
     public static void main(String[] args) {
+
+        ArrayList <Estudiante> integrantes = new ArrayList<Estudiante>();
         Persona miPersona = new Persona(1755014238,"Juan","Cordova","av de la prensa",77887);
         System.out.println(miPersona.getCI());
         Persona pedro = new Persona();
@@ -22,7 +25,14 @@ public class Menu {
         Diego.memorizar();
         Diego.leer();
         Diego.estudiar();
+        integrantes.add(Juan);
+        integrantes.add(Diego);
 
+        System.out.println("array y for ");
+        for (Estudiante integrante : integrantes){
+            System.out.println(integrante.getNombre() + " " + integrante.getApellido());
+            integrante.leer();
+        }
 
     }
 }
